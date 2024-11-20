e83c694e574e798792a98fbb59a6c1932a69e0ef
franciSauce <careyfrancisowino@gmail.com>
2024-11-20 12:36:10 +0300

    add basic app-flow

---
+++ app.js
@@ -0,0 +1,31 @@
+// Calculator object
+
+// Clear calculator values
+
+// Delete the last character from the curr-op
+
+// Append a number to the curr-op
+
+// Choose an operation & calculate
+
+// Function to display error message
+
+// Compute the result of the operation
+
+// Format the number for output
+
+// Update the output with current & previous operations
+
+// Function to handle button clicks
+
+// Add click event listeners to the buttons
+
+// Key mapping object for specific keys
+
+// Add keydown event listener with key
+
+// Handle numbers (0 - 9)
+
+// Handle special keys
+
+// Prevent default behavior for special keys
